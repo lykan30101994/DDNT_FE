4a1f6b53ab9f6bf2e7a112a2292bc238edf999c3
NhanHT <nhanht@rikkeisoft.com>
2024-09-23 15:07:58 +0700

    refactor code branch develop

--- src/components/layouts/footer/PageFooter.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <footer class="bg-light text-center p-4 mt-4">
-        <p>&copy; 2024 Copyright belongs to DDNT Team.</p>
-    </footer>
-</template>
-
--- src/components/layouts/header/PageHeader.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <header class="bg-primary text-white text-center p-4">
-    <h1>Tool gen testcase UT</h1>
-  </header>
-</template>
